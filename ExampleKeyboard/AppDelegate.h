e94bf36ca63506abf413dbcaeeb7638c76b4c758
David Mazza <david@fleksy.com>
2014-11-06 11:42:23 -0800

    Single view project created

---
+++ ExampleKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleKeyboard
+//
+//  Created by David Mazza on 11/6/14.
+//  Copyright (c) 2014 David Mazza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExampleKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleKeyboard
+//
+//  Created by David Mazza on 11/6/14.
+//  Copyright (c) 2014 David Mazza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
