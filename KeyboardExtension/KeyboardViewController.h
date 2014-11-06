b6b289939060efb232c313cae1190be3e5d20027
David Mazza <david@fleksy.com>
2014-11-06 11:51:33 -0800

    Added KeyboardExtension target and a text field to the container

---
+++ KeyboardExtension/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  KeyboardExtension
+//
+//  Created by David Mazza on 11/6/14.
+//  Copyright (c) 2014 David Mazza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
